4a1587139afa01a8af27238ea690ff0184d35781
suraj_122 <122suraj0@gmail.com>
2021-07-07 16:36:02 +0530

    refactor the code

--- block-BJaadh/code/index.js
+++ block-BJaadh/code/index.js
@@ -33,7 +33,7 @@ console.log(
 // - Find all the words that contain 'is' use string method 'indexOf'
 console.log(
   strings.filter((item) => {
-    return item.includes("is");
+    return item.indexOf("is") !== -1;
   })
 );
 // - Check if all the numbers in numbers array are divisible by three use array method (every)
@@ -53,9 +53,21 @@ console.log(
 // - Remove the last word in strings
 console.log(strings.pop());
 // - Find largest number in numbers
-
+console.log(
+  [...numbers]
+    .sort((a, b) => {
+      return a - b;
+    })
+    .pop()
+);
 // - Find longest string in strings
-
+console.log(
+  [...strings]
+    .sort((a, b) => {
+      return a.length - b.length;
+    })
+    .pop()
+);
 // - Find all the even numbers
 console.log(numbers);
 console.log(
@@ -73,18 +85,34 @@ console.log(
 // - Place a new word at the start of the array use (unshift)
 strings.unshift("Exactly");
 // - Make a subset of numbers array [18,9,7,11]
-numbers = [1, 12, 4, [18, 9, 7, 11], 3, 101, 5, 6, 9];
 // - Make a subset of strings array ['a','collection']
-strings = ["This", "is", ["a", "collection"], "of", "words"];
 
 // - Replace 12 & 18 with 1221 and 1881
-numbers[numbers.indexOf(12)] = 1221;
-numbers[numbers.flat(Infinity).indexOf(18)] = 1881;
-console.log(numbers);
+console.log(
+  numbers.map((item) => {
+    if (item === 12) {
+      return 1221;
+    } else if (item === 18) {
+      return 1881;
+    } else return item;
+  })
+);
 // - Replace words in strings array with the length of the word
-
+console.log(
+  strings.map((item) => {
+    return item.length;
+  })
+);
 // - Find the sum of the length of words using above question
-
+console.log(
+  strings
+    .map((item) => {
+      return item.length;
+    })
+    .reduce((acc, item) => {
+      return (acc += item);
+    }, 0)
+);
 // - Customers Array
 var customers = [
   { firstname: "Joe", lastname: "Blogs" },
@@ -99,18 +127,28 @@ console.log(
   })
 );
 // - Create new array with only first name
-let newArr = [];
-customers.forEach((item) => {
-  newArr.push(item.firstname);
+let firstName = customers.map((item) => {
+  return item.firstname;
 });
-console.log(newArr);
+
+console.log(firstName);
 // - Create new array with all the full names (ex: "Joe Blogs")
-let newFullArr = [];
-customers.forEach((item) => {
-  newFullArr.push(item.firstname + " " + item.lastname);
+let fullName = customers.map((item) => {
+  return item.firstname + " " + item.lastname;
 });
-console.log(newFullArr);
+console.log(fullName);
 // - Sort the array created above alphabetically
-newFullArr.sort();
-console.log(newFullArr);
+console.log(fullName.sort());
 // - Create a new array that contains only user who has at least one vowel in the firstname.
+let vowelName = customers.filter((item) => {
+  if (
+    item.firstname.toLowerCase().includes("a") ||
+    item.firstname.toLowerCase().includes("e") ||
+    item.firstname.toLowerCase().includes("i") ||
+    item.firstname.toLowerCase().includes("o") ||
+    item.firstname.toLowerCase().includes("u")
+  ) {
+    return true;
+  }
+});
+console.log(vowelName);
